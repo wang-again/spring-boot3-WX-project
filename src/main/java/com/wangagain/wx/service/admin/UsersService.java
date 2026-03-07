986b258fb56305bde8a00bbf4ef6e7aebbcec395
liuxingchina <admin@my123.org>
2026-03-07 20:04:41 +0800

    增加与数据库交互，以及修改相关登陆方法

--- src/main/java/com/wangagain/wx/service/admin/UsersService.java
+++ src/main/java/com/wangagain/wx/service/admin/UsersService.java
@@ -3,5 +3,6 @@ package com.wangagain.wx.service.admin;
 import com.wangagain.wx.utils.ResultLogin;
 
 public interface UsersService {
-    ResultLogin login(String name, String password);
+    ResultLogin login(String name, String password) throws Exception;
+    ResultLogin register(String name, String password) throws Exception;
 }
