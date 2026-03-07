0d41bd5c0561644b897d95c213f690581a662432
liuxingchina <admin@my123.org>
2026-03-08 00:21:43 +0800

    修正注册登陆反复加密bug,以及时区bug

--- src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
+++ src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
@@ -3,9 +3,9 @@ package com.wangagain.wx.service.impl.admin;
 import com.wangagain.wx.entity.Users;
 import com.wangagain.wx.mapper.UsersMapper;
 import com.wangagain.wx.service.admin.UsersService;
-import com.wangagain.wx.utils.AESUtil;
 import com.wangagain.wx.utils.ResultLogin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,32 +13,27 @@ public class UsersServiceImpl implements UsersService {
     private final String AES_TOKEN = "asdfa48210123456";
     @Autowired
     private UsersMapper usersMapper;
-    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public ResultLogin login(String name, String password) throws Exception {
         Users user = usersMapper.findUserExist(name);
-        String privatePassword = AESUtil.encrypt(password, AES_TOKEN);
-        if(user == null){
+        if (user == null) {
+            return new ResultLogin(1000, "用户名不存在", null);
             // 用户不存在
-            return new ResultLogin(1000,"用户名不存在", null);
-        }else{
-            // 用户存在
-            Users user1 = usersMapper.login(name,privatePassword);
-            if(user1 == null){
-                // 密码错误
-                return new ResultLogin(1001,"密码错误", null);
-            }
-            return new ResultLogin(1002,"登录成功", user1);
         }
+        if (passwordEncoder.matches(password, user.getUPwd())) return new ResultLogin(1001, "密码错误", null);
+        // 密码错误
+        return new ResultLogin(1002, "登录成功", user);
     }
 
-    @Override
-    public ResultLogin register(String name, String password) throws Exception {
-        String privatePassword = AESUtil.encrypt(password, AES_TOKEN);
-        int register = usersMapper.register(name, privatePassword);
-        if(register == 0){
-            return new ResultLogin(1000,"用户名存在", null);
-        }
-        return new ResultLogin(1001,"注册成功", null);
-    }
+@Override
+public ResultLogin register(String name, String password) throws Exception {
+    Users user = usersMapper.findUserExist(name);
+    if (user != null) return new ResultLogin(1000, "用户名已存在", null);
+    int rows = usersMapper.register(name, password);
+    if (rows > 0) return new ResultLogin(1002, "注册成功", null);
+    return new ResultLogin(1001, "注册失败", null);
+}
 }
