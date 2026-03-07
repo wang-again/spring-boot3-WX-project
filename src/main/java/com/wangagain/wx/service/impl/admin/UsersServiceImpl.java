5b34884df3131cfeb1ee7dc3b44397d824a17d48
liuxingchina <admin@my123.org>
2026-03-07 22:45:02 +0800

    代码简化

--- src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
+++ src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
@@ -13,30 +13,29 @@ public class UsersServiceImpl implements UsersService {
     private final String AES_TOKEN = "asdfa48210";
     @Autowired
     private UsersMapper usersMapper;
+
     @Override
     public ResultLogin login(String name, String password) throws Exception {
-        Users user = usersMapper.findUserExist(name);
         String privatePassword = AESUtil.encrypt(password, AES_TOKEN);
-        if(user == null){
-            // 用户不存在
-            return new ResultLogin(1000,"用户名不存在", null);
-        }else{
-            // 用户存在
-            Users user1 = usersMapper.login(name,privatePassword);
-            if(user1 == null){
-                // 密码错误
-                return new ResultLogin(1001,"密码错误", null);
-            }
-            return new ResultLogin(1002,"登录成功", user1);
-    }}
+        Users user = usersMapper.findUserExist(name);
+        if (user == null) return new ResultLogin(1000, "用户名不存在", null);
+        Users user1 = usersMapper.login(name, privatePassword);
+        if (user1 != null) {
+            return new ResultLogin(1002, "登录成功", user1);
+
+        }
+        return new ResultLogin(1001, "密码错误", null);
+    }
 
     @Override
     public ResultLogin register(String name, String password) throws Exception {
+        Users user = usersMapper.findUserExist(name);
+        if (user != null) return new ResultLogin(1000, "注册失败:用户名存在", null);
         String privatePassword = AESUtil.encrypt(password, AES_TOKEN);
         int register = usersMapper.register(name, privatePassword);
-        if(register == 0){
-            return new ResultLogin(1000,"用户名存在", null);
+        if (register >= 0) {
+            return new ResultLogin(1002, "注册成功", null);
         }
-        return new ResultLogin(1001,"注册成功", null);
+        return new ResultLogin(1001, "注册失败:原因未知", null);
     }
-    }
+}
