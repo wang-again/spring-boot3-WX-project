5d9d9ac0a2e98ea4e34a45f0db378ed21268bcd0
xianyuhs <79857573+xianyuhs@users.noreply.github.com>
2026-03-08 00:26:13 +0800

    终于好了

--- src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
+++ src/main/java/com/wangagain/wx/service/impl/admin/UsersServiceImpl.java
@@ -3,7 +3,6 @@ package com.wangagain.wx.service.impl.admin;
 import com.wangagain.wx.entity.Users;
 import com.wangagain.wx.mapper.UsersMapper;
 import com.wangagain.wx.service.admin.UsersService;
-import com.wangagain.wx.utils.AESUtil;
 import com.wangagain.wx.utils.ResultLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,7 +41,7 @@ public class UsersServiceImpl implements UsersService {
 
     @Override
     public ResultLogin register(String name, String password) throws Exception {
-        String privatePassword = AESUtil.encrypt(password, AES_TOKEN);
+        String privatePassword = password;
         int register = usersMapper.register(name, privatePassword);
         if(register == 0){
             return new ResultLogin(1000,"用户名存在", null);
